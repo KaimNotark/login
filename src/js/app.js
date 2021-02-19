b20c62b1f89f4cd63c3aba14ac2a9358815195c3
KaimNotark <kaimnotark@yandex.ru>
2021-02-19 09:33:31 +0800

    add authorisation

--- src/js/app.js
+++ src/js/app.js
@@ -7,23 +7,60 @@ import '../css/style.css';
 import UI from './config/ui.config.js';
 import { validate } from './helpers/validate.js';
 import { showInputError, removeInputError } from './views/form.js';
-import { login } from './services/auth.service.js';
+import { login, auth } from './services/auth.service.js';
 import { notify } from './views/notifications.js';
 import { getNews } from './services/news.service.js';
 
 const { form, inputEmail, inputPassword } = UI;
 const inputs = [inputEmail, inputPassword];
 
+const { auth: {
+  auth_form,
+  email,
+  password,
+  nickname,
+  first_name,
+  last_name,
+  phone,
+  gender_orientation,
+  city,
+  country,
+  date_of_birth_day,
+  date_of_birth_month,
+  date_of_birth_year,
+} } = UI;
+
+const authInputs = [
+  email,
+  password,
+  nickname,
+  first_name,
+  last_name,
+  phone,
+  gender_orientation,
+  city,
+  country,
+  date_of_birth_day,
+  date_of_birth_month,
+  date_of_birth_year,
+];
+
 // Events
 form.addEventListener('submit', e => {
   e.preventDefault();
-  onSubmit();
+  onFormSubmit();
+});
+
+auth_form.addEventListener('submit', e => {
+  e.preventDefault();
+  onAuthFormSubmit();
 });
 
 inputs.forEach(el => el.addEventListener('focus', () => removeInputError(el)));
 
 // Handlers
-async function onSubmit() {
+async function onFormSubmit() {
+
   const isValidForm = inputs.every(el => {
     const isValidInput = validate(el);
     if (!isValidInput) { showInputError(el); };
@@ -44,4 +81,27 @@ async function onSubmit() {
 
   }
 
+};
+
+async function onAuthFormSubmit() {
+  console.log('onAuthFormSubmit is RUN');
+  try {
+    await auth(
+      email.value,
+      password.value,
+      nickname.value,
+      first_name.value,
+      last_name.value,
+      phone.value,
+      gender_orientation.value,
+      city.value,
+      country.value,
+      date_of_birth_day.value,
+      date_of_birth_month.value,
+      date_of_birth_year.value,
+    );
+    auth_form.reset();
+  } catch (error) {
+    console.log("onAuthFormSubmit--ERROR", error);
+  }
 };
--- src/js/config/api.config.js
+++ src/js/config/api.config.js
@@ -1,6 +1,6 @@
 const API_ENV = {
   apiURL: 'https://mlp-demo.herokuapp.com/api/public',
-  authURL: 'https://mlp-demo.herokuapp.com/auth/signup',
+  authURL: 'https://mlp-demo.herokuapp.com/api/public/auth/signup',
 };
 
 export default API_ENV;
